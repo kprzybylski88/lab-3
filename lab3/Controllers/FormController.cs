a480a72c773179e2dbf61488ad485ca872a17ea7
Krzysztof Przybylski <kp@bytetile.com>
2021-11-06 14:39:29 +0100

    Initial check-in of module lab3

---
+++ lab3/Controllers/FormController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace lab3.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Created", model);
+            }
+            return View(model);
+        }
+    }
+}
